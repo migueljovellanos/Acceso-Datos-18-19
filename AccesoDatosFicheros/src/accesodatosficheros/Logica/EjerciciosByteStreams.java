d2d9dc6304117c4653b6e07e165d5bfe92c6cb22
Miguel Jovellanos Rodriguez <migueljove.mjrg@gmail.com>
2018-10-16 14:14:22 +0200

    contar densidad letras implementado con grafica html

--- AccesoDatosFicheros/src/accesodatosficheros/Logica/EjerciciosByteStreams.java
+++ AccesoDatosFicheros/src/accesodatosficheros/Logica/EjerciciosByteStreams.java
@@ -5,10 +5,16 @@
  */
 package accesodatosficheros.Logica;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -44,12 +50,15 @@ public class EjerciciosByteStreams {
         }
         return texto.toString();
     }
+
     /**
-     * Metodo que cifra o descifra un mensaje dependiendo del boolean 
+     * Metodo que cifra o descifra un mensaje dependiendo del boolean
+     *
      * @param textoACifrar texto que se desea cifrar o descifrar
      * @param codigoCifrado numero de caracteres que se desplaza cada caracter
      * @param descifrar boolean que indica si deseas cifrar o descifrar el texto
-     * @return el mensaje cifrado o descifrado dependiendo del parametro descifrar
+     * @return el mensaje cifrado o descifrado dependiendo del parametro
+     * descifrar
      */
     public String cifradoCesar(String textoACifrar, int codigoCifrado, boolean descifrar) {
         StringBuilder cifrado = new StringBuilder();
@@ -57,12 +66,102 @@ public class EjerciciosByteStreams {
             codigoCifrado = -codigoCifrado;
         }
         for (int i = 0; i < textoACifrar.length(); i++) {
-            if ((textoACifrar.charAt(i) >= 'a' && textoACifrar.charAt(i) <= 'z') || (textoACifrar.charAt(i) >= 'A' && textoACifrar.charAt(i) <= 'Z' )) {
+            if ((textoACifrar.charAt(i) >= 'a' && textoACifrar.charAt(i) <= 'z') || (textoACifrar.charAt(i) >= 'A' && textoACifrar.charAt(i) <= 'Z')) {
                 cifrado.append((char) (((textoACifrar.charAt(i) + 26 + codigoCifrado - 65) % 26) + 65));
-            }else {
+            } else {
                 cifrado.append((char) (textoACifrar.charAt(i)));
             }
         }
         return cifrado.toString();
     }
+
+    public Map<Character, Integer> contarDensidadDeCadaLetra(File fichero, File ficheroGrafica) throws MisExcepciones.NoExisteDirectorio, FileNotFoundException, IOException {
+        Map<Character, Integer> distribucionLetras = new HashMap<Character, Integer>();
+        if (!fichero.exists()) {
+            throw new MisExcepciones.NoExisteDirectorio();
+        }
+        FileReader fr = new FileReader(fichero);
+        BufferedReader br = new BufferedReader(fr);
+        String lineaLeida = br.readLine();
+        while (lineaLeida != null) {
+            for (int i = 0; i < lineaLeida.length(); i++) {
+
+                char letraLeida = Character.toLowerCase(lineaLeida.charAt(i));
+
+                Map<Character, Character> tablaReemplazos = new HashMap<>();
+                tablaReemplazos.put('á', 'a');
+                tablaReemplazos.put('é', 'e');
+                tablaReemplazos.put('í', 'i');
+
+                if (tablaReemplazos.get(letraLeida) != null) {
+                    letraLeida = tablaReemplazos.get(letraLeida);
+                }
+
+                if (Character.isAlphabetic(letraLeida)) {
+                    if (!distribucionLetras.containsKey(letraLeida)) {
+                        distribucionLetras.put(letraLeida, 1);
+                    } else {
+                        distribucionLetras.put(letraLeida, distribucionLetras.get(letraLeida) + 1);
+
+                    }
+                }
+            }
+            lineaLeida = br.readLine();
+        }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(ficheroGrafica));
+        String labels = "";
+        String datos = "";
+        for (Map.Entry<Character, Integer> entry : distribucionLetras.entrySet()) {
+            Character llave = entry.getKey();
+            Integer valor = entry.getValue();
+            labels = labels.concat("\"" + llave + "\",");
+            datos = datos.concat(valor.toString().concat(", "));
+
+        }
+
+        bw.write("<!doctype html>\n"
+                + "<html>\n"
+                + "    <head>\n"
+                + "        <title>Histograma Letras</title>\n"
+                + "        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js\"></script>\n"
+                + "        <style>\n"
+                + "\n"
+                + "        </style>\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        <div id=\"container\" style=\"width: 100%;\">\n"
+                + "            <canvas id=\"myChart\"></canvas>\n"
+                + "        </div>    \n"
+                + "        <script>\n"
+                + "            var ctx = document.getElementById('myChart').getContext('2d');\n"
+                + "            var chart = new Chart(ctx, {\n"
+                + "                // The type of chart we want to create\n"
+                + "                type: 'line',\n"
+                + "\n"
+                + "                // The data for our dataset\n"
+                + "                data: {\n"
+                + "                    labels: [" + labels + "],\n"
+                + "                    datasets: [{\n"
+                + "                            label: \"Histograma Letras\",\n"
+                + "                            backgroundColor: 'rgb(255, 0, 132)',\n"
+                + "                            borderColor: 'rgb(255, 99, 132)',\n"
+                + "                            data: [" + datos + "],\n"
+                + "                        }]\n"
+                + "                },\n"
+                + "\n"
+                + "                // Configuration options go here\n"
+                + "                options: {}\n"
+                + "            });\n"
+                + "        </script>\n"
+                + "\n"
+                + "    </body>\n"
+                + "\n"
+                + "</html>");
+
+        bw.flush();
+        bw.close();
+        return distribucionLetras;
+    }
+
 }
